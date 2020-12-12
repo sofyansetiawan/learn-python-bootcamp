3834ea1d7295d7a8b760a11ecacff0914c9a168a
Sofyan Setiawan <sofyansetiawannn@gmail.com>
2020-12-13 01:55:55 +0700

    learning at saturday night3

---
+++ 02-intermediate/errors.py
@@ -0,0 +1,39 @@
+# SYNTAX ERROR
+# ------------
+
+# print("hello"
+# a 3
+# a = "Hello" +
+
+# Indentation Error
+# -----------------
+
+# def funct1(first, last):
+# full = first + " " + last
+#     return print(full)
+
+# a = "Sofyan"
+# b = "Setiawan"
+
+# funct1(a, b)
+
+# if("a" == "a"):
+# print("Sesuai")
+
+# Type Error
+# ----------
+
+# x = 2 + "d" # error TypeError: unsupported operand type(s) for +: 'int' and 'str'
+
+# a = input("masukkan angka 1: ")
+# b = input("masukkan angka 2: ")
+# c = (a + b) + 2 # harus diubah ke int()
+# print(c)
+
+# Name Error
+# ----------
+# print(x) # x tidak ada disini
+# def function1(a): 
+#     print(a)
+
+# functio(100) # salah panggil nama function
